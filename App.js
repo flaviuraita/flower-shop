b223f161f7c7170fd8cbf41fb136df952ab44837
flaviuraita <flaviu.raita@wolfpack-digital.com>
2024-01-04 13:09:14 +0200

    Add order fetching

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    const response = await fetch('http://demo9812995.mockable.io/flowers');
+    const json = await response.json();
+    setOrders(json);
+  }
+
+  useEffect(() => {
+    getOrders();
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
