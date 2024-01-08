31f3744410e6e77ce46b4ec8468aae22b6fd6320
flaviuraita <flaviu.raita@wolfpack-digital.com>
2024-01-08 15:17:13 +0200

    Use SectionList for flowers list

--- src/components/FlowerStock.tsx
+++ src/components/FlowerStock.tsx
@@ -1,6 +1,6 @@
 import { getFlowers } from "../api/flowerService";
 import { useEffect, useState } from "react";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { Image, SectionList, StyleSheet, Text, View } from "react-native";
 
 const FlowerItem = ({flower}) => {
   return (
@@ -17,23 +17,46 @@ const FlowerItem = ({flower}) => {
 }
 
 export default function FlowerStock() {
-  const [flowers, setFlowers] = useState([])
-
-  const setFlowerState = async () => {
-    const flowers = await getFlowers();
-    setFlowers(flowers);
-  }
+  const [flowers, setFlowers] = useState([]);
 
   useEffect(() => {
     setFlowerState();
   }, [])
 
+  const setFlowerState = async () => {
+    const flowers = await getFlowers();
+    const processedFlowers = processFlowersData(flowers);
+
+    setFlowers(processedFlowers);
+  }
+
+  const processFlowersData = (flowers) => {
+    const processedFlowers = [];
+
+    Object.keys(flowers).map(flower => {
+      const index = processedFlowers.findIndex(f => f?.flower)
+      if (index > 0) {
+        processedFlowers[index].data.push(flowers[flower])
+      } else {
+        processedFlowers.push({
+          title: flower,
+          data: flowers[flower]
+        })
+      }
+    });
+
+    return processedFlowers;
+  }
+
   return (
     <>
-      {flowers && <FlatList
-        data={flowers}
+      {flowers && <SectionList
+        sections={flowers}
         renderItem={((flower) => <FlowerItem flower={flower.item}/>)}
         keyExtractor={flower => flower.id}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.header}>{title} flowers</Text>
+        )}
       />}
     </>
   )
@@ -52,4 +75,7 @@ const styles = StyleSheet.create({
   flowerItemLarge: {
     flex: 2
   },
+  header: {
+    textTransform: 'capitalize',
+  }
 })
